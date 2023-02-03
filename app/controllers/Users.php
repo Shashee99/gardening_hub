33f1a618c0153a1fc524c1fcbe23a165e0f95da0
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-02-03 13:49:48 +0530

    changed

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -448,7 +448,7 @@ class Users extends Controller
                         }
                         else
                         {
-                            $data['u_name_err'] = 'Your registarion is pending';
+                            $data['u_name_err'] = 'Your registration is pending';
                             $this->view('users/login',$data);
                         }
                     }
