46859de20b1635276af7666930d3c06d9b78e5c7
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-04-30 12:01:16 +0530

    changed dashbord

--- app/views/admin/home.php
+++ app/views/admin/home.php
@@ -2,100 +2,43 @@
 <h2>Registered users in the system.</h2>
 <div class="userlist">
 
-    <div class="userlist-usercard">
-        <div class="user">
-            <h3>Customers</h3>
-            <h1 class="usercount" id="customers"></h1>
-        </div>
-        <div class="userphoto">
-            <img src="<?= URLROOT; ?>/img/admin/customers.png" alt="">
-        </div>
-    </div>
-    <div class="userlist-usercard">
-        <div class="user">
-            <h3>Sellers</h3>
-            <h1 class="usercount" id="sellers"></h1>
-        </div>
-        <div class="userphoto" style="margin-left: 20px">
-            <img src="<?= URLROOT; ?>/img/admin/sellers.png" alt="">
-        </div>
-    </div>
-    <div class="userlist-usercard">
-        <div class="user">
-            <h3>Advisors</h3>
-            <h1 class="usercount" id="advisors"></h1>
-        </div>
-        <div class="userphoto" style="margin-left: 30px">
-            <img src="<?= URLROOT; ?>/img/admin/advisor.png" alt="">
-        </div>
-    </div>
-</div>
-<hr>
-<div class="newAdvisors">
-    <div class="newAdvisors-usercount">
-        <h2>New Advisors</h2>
-        <h4>Request</h4>
-        <h4 id="newadcount"></h4>
-    </div>
+   <div class="barcontainer">
+       <canvas id="userChart"></canvas>
+       <script>
 
-    <div class="advisorcards" id="latestadvisors">
+           var ctx = document.getElementById('userChart').getContext('2d');
+           var userChart = new Chart(ctx, {
+               type: 'bar',
+               data: {
+                   labels: ['Customers', 'Advisors', 'Sellers'],
+                   datasets: [{
+                       label: 'Number of Users',
+                       data: [10, 20, 30],
+                       backgroundColor: [
+                           'rgba(54, 162, 235, 1)'
+                       ],
+                       borderColor: [
 
+                           'rgba(54, 162, 235, 1)'
 
+                       ],
+                       borderWidth: 1
+                   }]
+               },
+               options: {
+                   scales: {
+                       yAxes: [{
+                           ticks: {
+                               beginAtZero: true,
+                               max: 50
 
-    </div>
-
-    <div class="afteradvisors">
-        <p> Click see more to view all Advisors</p>
-        <div class="seemore">
-            <h4>See more</h4>
-        </div>
-    </div>
-
-    <hr>
-
-</div>
-<div class="newAdvisors">
-    <div class="newAdvisors-usercount">
-        <h2>New Sellers</h2>
-        <h4>Request</h4>
-        <h4 id="newsellCount"></h4>
-    </div>
-
-    <div class="advisorcards" id="latestsellers">
-
-
-
-    </div>
-
-    <div class="afteradvisors">
-        <p> Click see more to view all Sellers</p>
-        <div class="seemore">
-            <h4>See more</h4>
-        </div>
-    </div>
-
-    <hr>
-
-</div>
-<div class="newAdvisors">
-    <div class="newAdvisors-usercount">
-        <h2>Customers</h2>
-
-    </div>
-    <div class="advisorcards" id="latestcustomers">
-
-
-
-    </div>
-
-    <div class="afteradvisors">
-        <p> Click see more to view all Sellers</p>
-        <div class="seemore">
-            <h4>See more</h4>
-        </div>
-    </div>
-
-    <hr>
+                           }
+                       }]
+                   }
+               }
+           });
+       </script>
+   </div>
 
 </div>
 
