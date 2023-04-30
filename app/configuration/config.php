335615589a57f3910eed63dedd6b11c7ec88dc09
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-04-30 12:30:50 +0530

    commit before pull

--- app/configuration/config.php
+++ app/configuration/config.php
@@ -4,7 +4,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'gardening_hb');
+    define('DB_NAME', 'gardening_hub');
 
     // Define app root
     define('APPROOT', dirname(dirname(__FILE__)));
