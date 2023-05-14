417dc3a897fba8d4da101b04997a7087c6e4d07b
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-05-14 18:11:04 +0530

    modified homepage 2

--- app/views/pages/customers.php
+++ app/views/pages/customers.php
@@ -5,13 +5,28 @@
             <h1>Garden with ease. Grow with us.</h1>
 
             <div class="advisordecription" style="padding-bottom: 30px">
-                <div class="advisorsimags">
+                <div class="advisorsimags" style="margin-top: 50px">
                     <img src="<?= URLROOT;?>/img/landingPage/farmer.jpg" style="border-radius: 50px" alt="" width="100%" height="100%" >
                 </div>
                 <div class = "adviosrdescriptiontext">
-                    <p style="font-size: 25px; padding-top: 50px;" >
+                    <p style="font-size: 25px; padding-top: 100px;" >
                         <i>Welcome to our vibrant community of passionate gardeners!<br>
-                            Our system empowers you to explore the world of gardening like never before. As a customer in our system, you have access to an array of features that make gardening easy, enjoyable, and rewarding. You can search for sellers to find the best high-quality products, post your gardening problems in our community, and receive expert advice from qualified advisors. Looking to connect with like-minded gardeners? Our system also allows you to find and chat with nearest gardening friends to share your experiences and insights.
+                            Our system empowers you to explore the world of gardening like never before. As a customer in our system, you have access to an array of features that make gardening easy, enjoyable, and rewarding. You can search for sellers to find the best high-quality products, post your gardening problems in our community, and receive expert advice from qualified advisors.
+                           </i>
+                    </p>
+                </div>
+
+            </div>
+            <br>
+
+
+
+        </section>
+        <section style="height: 100vh" class="advisordec" style = "margin-top: 10px; margin-bottom:110px">
+            <div class="advisordecription_converse" >
+                <div class = "adviosrdescriptiontext">
+                    <p style="font-size: 25px; padding-top: 20px;" >
+ Looking to connect with like-minded gardeners? <br> Our system also allows you to find and chat with nearest gardening friends to share your experiences and insights.
                             <br> With our user-friendly platform and cutting-edge technologies, you can take your gardening journey to new heights.
                             <br>Join our community today and let's grow together!</i>
                     </p>
@@ -20,10 +35,17 @@
                         <h3 style="font-size: 30px">sign up now !</h3>
                     </a>
                 </div>
+                <div class="advisorsimags">
+                    <img src="<?= URLROOT;?>/img/landingPage/farm2.jpg" style="border-radius: 50px" alt="" width="100%" height="100%" >
+                </div>
 
             </div>
+
+
+
 
         </section>
 
     </div>
+    <br><br><br><br><br>
 <?php require APPROOT . '/views/inc/incHome/footer.php'; ?>
